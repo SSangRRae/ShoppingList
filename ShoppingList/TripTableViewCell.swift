bb50402825df7584134494f4c23235f3bdf0b069
SSangRRae <pepe0003@kakao.com>
2024-01-08 21:14:21 +0900

    feat: UI

---
+++ ShoppingList/TripTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TripTableViewCell.swift
+//  ShoppingList
+//
+//  Created by SangRae Kim on 1/8/24.
+//
+
+import UIKit
+
+class TripTableViewCell: UITableViewCell {
+    
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
---
+++ ShoppingList/TripTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TripTableViewController.swift
+//  ShoppingList
+//
+//  Created by SangRae Kim on 1/8/24.
+//
+
+import UIKit
+
+class TripTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
